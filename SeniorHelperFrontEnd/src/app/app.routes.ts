504ce3162345f7d690fb8e4be1b00dcf74249e1a
dewey mccrary <S3800528@students.fscj.edu>
2026-02-11 19:09:50 -0500

    started calendar features

--- SeniorHelperFrontEnd/src/app/app.routes.ts
+++ SeniorHelperFrontEnd/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { EducationComponent } from './components/education/education.component';
+import { Calendar } from './components/calendar/calendar';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'education', component: EducationComponent },
+  { path: 'calendar', component: Calendar },
   { path: '**', redirectTo: 'login' }
 ];
